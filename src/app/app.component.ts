1a85c5806d59eb9eac56e7325a033c69c600ab31
erikwiklander <erik.wiklander@wiklandia.com>
2017-10-22 21:23:38 +0200

    Added Angular Material and did some initial calls to backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  response: any[];
+  lat: number;
+  lon: number;
+
+  constructor(private http: HttpClient) {}
+
+  onClickGetStations() {
+    console.log('Hejsan!', this.http);
+    this.http.get<any[]>('http://localhost:8080/station/stops').subscribe(data => {
+      console.log(data);
+      this.response = data;
+    });
+  }
+
+  onClickGetClosest() {
+    console.log('Closest');
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.lat = position.coords.latitude;
+        this.lon = position.coords.longitude;
+      },
+      error => console.log('Error: ', error));
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule, MatToolbarModule, MatSelectModule, MatButtonModule, NoConflictStyleCompatibilityMode } from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +15,16 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatSelectModule,
+    MatButtonModule,
+    NoConflictStyleCompatibilityMode,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
